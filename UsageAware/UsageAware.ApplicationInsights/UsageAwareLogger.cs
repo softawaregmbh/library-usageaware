afe22c351e27ec1724b4602f02d5b4b469e1101a
Roman Schacherl <roman.schacherl@softaware.at>
2018-04-10 15:48:09 +0200

    Initial structure

---
+++ UsageAware/UsageAware.ApplicationInsights/UsageAwareLogger.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UsageAware.ApplicationInsights;
+
+namespace UsageAware.ApplicationInsights
+{
+    public class UsageAwareLogger : IUsageAwareLogger
+    {
+        public Task LoginAsync(string userId, string tenant = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UsageAware/UsageAware/IUsageAwareLogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsageAware
+{
+    public interface IUsageAwareLogger
+    {
+        Task LoginAsync(string userId, string tenant = null);
+    }
+}
